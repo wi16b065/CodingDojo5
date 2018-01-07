17db87cfeb9f25148a87e3a7233d1bd20a62991b
Robert Chochole <wi16b065@technikum-wien.at>
2018-01-07 10:17:22 +0100

    Added Cart

--- CodingDojo5/ViewModel/MainViewModel.cs
+++ CodingDojo5/ViewModel/MainViewModel.cs
@@ -38,6 +38,8 @@ namespace CodingDojo5.ViewModel
 
         public ObservableCollection<ItemVm> Category { get; set; }
 
+        public ObservableCollection<ItemVm> Cart { get; set; }
+
         public MainViewModel()
         {
             ////if (IsInDesignMode)
@@ -49,6 +51,7 @@ namespace CodingDojo5.ViewModel
             ////    // Code runs "for real"
             ////}
             Category = new ObservableCollection<ItemVm>();
+            Cart = new ObservableCollection<ItemVm>();
             GenerateDemoData();
         }
         
@@ -56,7 +59,7 @@ namespace CodingDojo5.ViewModel
         {
             //generate Categories
             Category.Add(new ItemVm("My LEGO", "", new BitmapImage(new Uri("Images/lego1.jpg", UriKind.Relative))));
-            Category.Add(new ItemVm("My Playmobile", "", new BitmapImage(new Uri("Images/playmobil1.jpg", UriKind.Relative))));
+            Category.Add(new ItemVm("My Playmobil", "", new BitmapImage(new Uri("Images/playmobil1.jpg", UriKind.Relative))));
 
             //Add lego items to category
             Category[0].AddItemToCategory(new ItemVm("Helicopter", "10+", new BitmapImage(new Uri("Images/lego4.jpg", UriKind.Relative))));
@@ -65,9 +68,17 @@ namespace CodingDojo5.ViewModel
             Category[0].AddItemToCategory(new ItemVm("Crawler Crane", "12+", new BitmapImage(new Uri("Images/lego4.jpg", UriKind.Relative))));
             
             //Add playmobile items to category
-            Category[1].AddItemToCategory(new ItemVm("Beach House", "5+", new BitmapImage(new Uri("Images/playmobile1.jpg", UriKind.Relative))));
-            Category[1].AddItemToCategory(new ItemVm("House", "8+", new BitmapImage(new Uri("Images/playmobile2.jpg", UriKind.Relative))));
-            Category[1].AddItemToCategory(new ItemVm("Knights", "8+", new BitmapImage(new Uri("Images/playmobile13.jpg", UriKind.Relative))));
+            Category[1].AddItemToCategory(new ItemVm("Beach House", "5+", new BitmapImage(new Uri("Images/playmobil1.jpg", UriKind.Relative))));
+            Category[1].AddItemToCategory(new ItemVm("House", "8+", new BitmapImage(new Uri("Images/playmobil2.jpg", UriKind.Relative))));
+            Category[1].AddItemToCategory(new ItemVm("3 Knights", "8+", new BitmapImage(new Uri("Images/playmobil3.jpg", UriKind.Relative))));
+            Category[1].AddItemToCategory(new ItemVm("House 1", "10+", new BitmapImage(new Uri("Images/playmobil2.jpg", UriKind.Relative))));
+            Category[1].AddItemToCategory(new ItemVm("5 Knights", "8+", new BitmapImage(new Uri("Images/playmobil3.jpg", UriKind.Relative))));
+            Category[1].AddItemToCategory(new ItemVm("House 2", "12+", new BitmapImage(new Uri("Images/playmobil2.jpg", UriKind.Relative))));
+            Category[1].AddItemToCategory(new ItemVm("7 Knights", "8+", new BitmapImage(new Uri("Images/playmobil3.jpg", UriKind.Relative))));
+
+            //Testitem for Cart
+            Cart.Add(new ItemVm("Crawler Crane", "12+", new BitmapImage(new Uri("Images/lego4.jpg", UriKind.Relative))));
+            Cart.Add(new ItemVm("Digger", "12+", new BitmapImage(new Uri("Images/lego2.jpg", UriKind.Relative))));
         }
     }
 }
